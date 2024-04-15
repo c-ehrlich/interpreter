877863638eb977948495a8d5974a24968a5b7623
c-ehrlich <ehrlich.christopher@gmail.com>
2024-04-15 21:02:59 +0200

    variables work

--- mine/evaluator/evaluator_test.go
+++ mine/evaluator/evaluator_test.go
@@ -11,8 +11,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
@@ -234,6 +235,10 @@ if (10 > 1) {
 }`,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -250,5 +255,21 @@ if (10 > 1) {
 			t.Errorf("wrong error message. expected=%q, got=%q",
 				tt.expectedMessage, errorObj.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
