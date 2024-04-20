390ae9cd90a5c21ee9df89253d3a9dc90b52db2c
c-ehrlich <ehrlich.christopher@gmail.com>
2024-04-20 22:20:19 +0200

    retrieve from hash

--- mine/evaluator/evaluator.go
+++ mine/evaluator/evaluator.go
@@ -385,6 +385,8 @@ func evalIndexExpression(
 	switch {
 	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
 		return evalArrayIndexExpression(left, index)
+	case left.Type() == object.HASH_OBJ:
+		return evalHashIndexExpression(left, index)
 	default:
 		return newError("index operator not supported: %s", left.Type())
 	}
@@ -431,4 +433,22 @@ func evalHashLiteral(
 	}
 
 	return &object.Hash{Pairs: pairs}
+}
+
+func evalHashIndexExpression(
+	hash, index object.Object,
+) object.Object {
+	hashObject := hash.(*object.Hash)
+
+	key, ok := index.(object.Hashable)
+	if !ok {
+		return newError("unusable as hash key: %s", index.Type())
+	}
+
+	pair, ok := hashObject.Pairs[key.HashKey()]
+	if !ok {
+		return NULL
+	}
+
+	return pair.Value
 }
