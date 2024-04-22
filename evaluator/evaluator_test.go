f850e57ac0dacfe5836eb343fb132ae66658120f
c-ehrlich <ehrlich.christopher@gmail.com>
2024-04-22 23:17:54 +0200

    evaluate modulo

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -35,6 +35,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"3 * 3 * 3 + 10", 37},
 		{"3 * (3 * 3) + 10", 37},
 		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
+		{"2 % 1", 0},
+		{"3 % 2", 1},
 	}
 
 	for _, tt := range tests {
